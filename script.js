07320fe906310ee4a030441e6584efe4cd1acd82
MayuriBadgujar <120549440+MayuriBadgujar@users.noreply.github.com>
2023-05-15 21:30:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function threeSum(arr, target) {
 // write your code here
+	if (ind == n) {
+        if (count == 3) {
+            if (Math.abs(x - sum) < minm[0]) {
+                minm[0] = Math.abs(x - sum);
+                ans[0] = sum;
+            }
+        }
+        return;
   
 }
 
